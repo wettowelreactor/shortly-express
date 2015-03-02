0f05b752dd968542d73f0800efa16c264611d8fa
wettowelreactor <camille@rousselfamily.com>
2015-03-02 15:50:01 -0800

    First take at User model with initialize function.

--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,27 @@
 var db = require('../config');
+var Link = require('./link');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
+var bcrypt = Promise.promisifyAll(require('bcrypt'));
 
 var User = db.Model.extend({
+  tableName: 'users',
+  urls: function() {
+    return this.hasMany(Link);
+  },
+  initialize: function(){
+    this.on('creating', function(model){
+      var password = model.get('password');
+      model.unset('password');
+      bcrypt.hash(password, null, null).then(function(err, hash){
+        if (err) {
+          console.log('Somthing went wrong in hashing the password', err);
+        } else {
+          model.set('hash', hash);
+        }
+      });
+    });
+  },
 });
 
 module.exports = User;
